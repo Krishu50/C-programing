e4cb32a3d7e9f8f67bdbd46f17dad8ac165105a4
Krishu50 <khuntkrishu50@gmail.com>
2025-07-28 21:57:37 +0530

    Add files via upload

---
+++ practical 1/BIO-DATA.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+printf("\n# = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = ");
+printf("\n\t\t\t\t        BIO-DATA\t\t       \n  ");
+printf("\n# = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = =");
+printf("\n\t\t   ");
+printf("\n\t\t\t\t          Basic information\t\t        ");
+printf("\n\t\t\t\t          ..................\t\t        ");
+printf("\n\t\t   ");
+printf("\n\t\t\t\t   Name     :   Krishu khunt   ");
+printf("\n\t\t\t\t   Address  :   surat");
+printf("\n\t\t\t\t   mobile no:   6351516031");
+printf("\n\t\t\t\t   Gender   :   Female");
+printf("\n\t\t\t\t   DOB      :   12/04/2008");
+printf("\n\t\t   ");
+printf("\n\t\t\t\t              Education Qualification \t\t  ");
+printf("\n\t\t\t\t              .......................  \t\t ");
+printf("\n\t   ");
+printf("\n\t\t\t\t  Degree         Name of school          passing year");
+printf("\n\t\t\t\t  SSC            MAUNI INTERNATIONAL          2023");
+printf("\n\t\t\t\t  HSC            ASHADEEP IIT                 2025");
+printf("\n\t\t");
+printf("\n\t\t\t\t              OTher information \t\t");
+printf("\n\t\t\t\t              ................. \t\t");
+printf("\n\t\t");
+printf("\n\t\t\t      Technical skill:   C,C++");
+printf("\n\t\t\t      Hobbies        :   Drawing,Travelling");
+printf("\n\t\t");
+printf("\n# = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = =");
+printf("\n\t\t\t\t          THANK YOU \t\t                ");
+printf("\n# = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = = # = = = = =");
+return 0;
+}
