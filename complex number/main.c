908855c90f54d6f2dfc968a8743a8cdff0ff5b53
Krishu50 <khuntkrishu50@gmail.com>
2025-07-30 09:09:19 +0530

    Add files via upload

---
+++ complex number/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int real,imaginary;
+    printf("enter the complex number in a form of a+bi");
+    scanf("%d+%di",&real, &imaginary);
+    printf("real number=%d\n",real);
+    printf("imaginary number=%d\n",imaginary);
+    return 0;
+}
